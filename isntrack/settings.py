7038ff9a6fc14066d542e915375049c2af3d87dd
Chen <leeyuchen0825@gmail.com>
2024-02-18 19:24:23 +0000

    Removed secret key

--- isntrack/settings.py
+++ isntrack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@4cv(=@o05+ny$y2-_d@fq-&c=&9cppr#(w9zr)n-$zyzh70dk' 
+SECRET_KEY = '' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
